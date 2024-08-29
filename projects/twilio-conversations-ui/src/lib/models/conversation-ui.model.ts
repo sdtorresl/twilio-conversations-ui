819109e87ec3c56ac58fc15135d6e2434a6e5093
Sergio Torres <setorres@twilio.com>
2024-08-29 18:05:23 -0500

    Rmove last message

--- projects/twilio-conversations-ui/src/lib/models/conversation-ui.model.ts
+++ projects/twilio-conversations-ui/src/lib/models/conversation-ui.model.ts
@@ -1,43 +1,54 @@
-import { Conversation, Participant } from '@twilio/conversations';
+import { Conversation, Message, Participant } from '@twilio/conversations';
 
 export class ConversationUi {
+  conversation: Conversation;
+  _unreadMessages: number | undefined | null = 0;
+  _displayName: string = '';
 
-    conversation: Conversation;
-    _displayName: string = '';
+  constructor(conversation: Conversation) {
+    this.conversation = conversation;
+    conversation.getUnreadMessagesCount().then((count) => {
+      this._unreadMessages = count;
+    });
+  }
 
-    constructor(conversation: Conversation) {
-        this.conversation = conversation;
+  get unreadMessages(): number {
+    return this._unreadMessages ?? 0;
+  }
+
+  get displayName(): string {
+    if (this.conversation.friendlyName != null) {
+      return this.conversation.friendlyName;
     }
 
-    get displayName(): string {
-        if (this.conversation.friendlyName != null) {
-            return this.conversation.friendlyName;
-        }
+    const participants: Map<string, Participant> =
+      this.conversation._participants;
 
-        const participants: Map<string, Participant> = this.conversation._participants;
+    let identitiesString = '';
 
-        let identitiesString = '';
+    participants.forEach((participant: Participant) => {
+      if (participant.identity) {
+        identitiesString += `${participant.identity}, `;
+      } else {
+        identitiesString += `${participant.bindings}, `;
+      }
+    });
 
-        participants.forEach((participant: Participant) => {
-            if (participant.identity) {
-                identitiesString += `${participant.identity}, `;
-            } else {
-                identitiesString += `${participant.bindings}, `;
-            }
-        });
-
-        if (identitiesString.length > 0) {
-            identitiesString = identitiesString.slice(0, -2);
-        }
-
-        return identitiesString;
+    if (identitiesString.length > 0) {
+      identitiesString = identitiesString.slice(0, -2);
     }
 
-    get displayLastMessage(): string {
-        // TODO: get last message
-        const lastMesasge = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, necessitatibus maiores at doloremque exercitationem pariatur itaque amet sit autem illo voluptatum nostrum, hic quae ab architecto, modi nam adipisci. Reiciendis.';
+    return identitiesString;
+  }
 
+  async displayLastMessage(): Promise<string | null | undefined> {
+    return this.conversation.getMessages().then((paginator) => {
+      const messages: Message[] = paginator.items;
+      if (messages.length > 0) {
+        return messages[messages.length - 1]?.body;
+      }
 
-        return lastMesasge;
-    }
+      return undefined;
+    });
+  }
 }
